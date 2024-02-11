ad05b869c282391fc54a9b8149f326075d41a738
Fil31 <lolid76@gmail.com>
2024-02-11 14:13:29 +0200

    add domain and dto

---
+++ CarsSaaber.Core/Dto/CarsDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CarsSaaber.Core.Dto
+{
+    public class CarsDto
+    {
+        public Guid? Id { get; set; }
+        public int Price { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public int Discount { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
